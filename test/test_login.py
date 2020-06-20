b5a6bece8079ce91f0527bade30cc7f5b715c2f8
ElminaIusifova <32398063+ElminaIusifova@users.noreply.github.com>
2020-06-20 00:51:31 -0400

    test failedtest

--- test/test_login.py
+++ test/test_login.py
@@ -10,6 +10,7 @@ class Azercell(TestCase):
         cls.browser_driver.get('http://azercell.com/my/login')
         cls.browser_driver.find_element_by_css_selector('li.b-nav__list:nth-child(3) > a:nth-child(1)').click()
 
+#PASSED
     def test_mobile_number_not_filled(self):
         self.browser_driver.get('http://azercell.com/my/login')
         self.browser_driver.find_element_by_css_selector('#mat-input-1').click()
@@ -21,8 +22,7 @@ class Azercell(TestCase):
         actual_warning = self.browser_driver.find_element_by_css_selector('#mat-error-0').text
         assert expected_warning==actual_warning
 
-
-
+#PASSED
     def test_enter_number_without_prefix(self):
         self.browser_driver.get('http://azercell.com/my/login')
         self.browser_driver.find_element_by_css_selector('#mat-input-1').send_keys('8179001')
@@ -32,7 +32,7 @@ class Azercell(TestCase):
         assert expected_warning==actual_warning
 
 
-
+#PASSED
     def test_enter_short_number(self):
         self.browser_driver.get('http://azercell.com/my/login')
         self.browser_driver.find_element_by_css_selector('#mat-input-1').send_keys('51817900')
@@ -41,6 +41,7 @@ class Azercell(TestCase):
         actual_warning = self.browser_driver.find_element_by_css_selector('#mat-error-3').text
         assert expected_warning == actual_warning
 
+#FAILED! WRONG TEST CASE
     def test_enter_long_number(self):
         self.browser_driver.get('http://azercell.com/my/login')
         self.browser_driver.find_element_by_css_selector('#mat-input-1').send_keys('0518179001')
@@ -49,6 +50,7 @@ class Azercell(TestCase):
         actual_warning = self.browser_driver.find_element_by_css_selector('#mat-error-3').text
         assert expected_warning == actual_warning
 
+#FAILED
     def test_enter_number_with_wrong_prefix(self):
         self.browser_driver.get('http://azercell.com/my/login')
         self.browser_driver.find_element_by_css_selector('#mat-input-1').send_keys('058179001')
@@ -57,7 +59,16 @@ class Azercell(TestCase):
         actual_warning = self.browser_driver.find_element_by_css_selector('#mat-error-2').text
         assert expected_warning == actual_warning
 
+#FAILED
+    def test_enter_none_numeric_symbols_to_mobile_number_field(self):
+        self.browser_driver.get('http://azercell.com/my/login')
+        self.browser_driver.find_element_by_css_selector('#mat-input-1').send_keys('51EiA9@$1')
+        self.browser_driver.find_element_by_css_selector('#mat-input-2').click()
+        expected_input=int
+        actual_input=self.browser_driver.find_element_by_css_selector('#mat-input-1').get_attribute('value')
+        assert expected_input == actual_input
 
+#PASSED
     def test_login_with_registered_user_using_correct_credentials(self):
         self.browser_driver.get('http://azercell.com/my/login')
         self.browser_driver.find_element_by_css_selector('#mat-input-1').click()
